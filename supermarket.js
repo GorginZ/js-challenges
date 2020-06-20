43db401601770242c79a0052b7e20803075ab195
GorginZ <georgia.z.leng@gmail.com>
2020-06-20 15:06:54 +1000

    supermarket challenge

---
+++ supermarket.js
@@ -0,0 +1,82 @@
+// # Supermarket
+
+// There is a queue for the self-checkout tills at the supermarket. 
+// Your task is write a function to calculate the total time required for 
+
+// all the customers to check out!
+
+
+
+// The function has two input variables:
+
+//     customers: an array of positive integers 
+
+//             representing the queue. Each integer represents a customer, 
+
+//             and its value is the amount of time they require to check out.
+
+//     n: a positive integer, the number of checkout tills.
+
+
+
+// The function should consider each person in order, and add them to the 
+
+// 'smallest' line at that time (the line with the least total time).
+
+
+
+// The function should return an integer, the time it takes for all lines to empty.
+
+
+
+// For example, if the input queue is [1,2,3,4,5,6] and there are 2 tills,
+
+// the queue would be separated as follows:
+
+// till1       till2
+
+// 1           2
+
+// 3           4
+
+// 5           6
+
+// ---         ---
+
+// 9           12
+
+
+
+// So this would take 12 total time, since all customers will be served when
+
+// the last customer at till2 is served.
+
+
+
+// Returns the queue time for all customers given number of tills
+function queueTime(customers, n) {
+    let newCust = []
+    let tills = []
+    let smaller = Math.min(...tills)
+    let time = 0
+    //array with two indexes values of which represent time /items being processed by tills
+    for (var i = 1; i<= n; i++) {
+        tills.push(0)
+    }
+    //works through line of customers taking first from each
+    //smaller has to be redefined at beginning of loop otherwise it keeps allocating to the original 'smaller'
+    //removes each customer as it is removed
+    //re define time at end of loop to update time
+    while (customers.length > 0) {
+        smaller = Math.min(...tills)
+        let replace = tills.indexOf(smaller)
+        tills[replace] += customers[0]
+        newCust = customers.splice(0,1)    
+        time = Math.max(...tills) 
+    }  
+    console.log(tills)
+    return time
+}
+
+console.log(queueTime([],5))
+module.exports = {queueTime}
