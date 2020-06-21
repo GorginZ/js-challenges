bbaf7fdaa9d4b1596e1dc977c5e2023c66ed93bd
GorginZ <georgia.z.leng@gmail.com>
2020-06-21 18:43:50 +1000

    lodash

--- is_match/index.js
+++ is_match/index.js
@@ -1,18 +1,27 @@
 // harrison will show you how to run the tests and also what lodash is
 // https://www.npmjs.com/browse/depended
 
+
 /*
 Is Match
 Tells you if the keys and values in properties are contained in object.
 Example:
-var stooge = {name: 'moe', age: 32};
-isMatch(stooge, {age: 32});
+
 => true
 */
 
 const isMatch = (objOne, objTwo) => {
-  // Your code here!
-  return true
+for (let key in objOne) {
+  if (objTwo.hasOwnProperty(key)) {
+    if (objOne[key] === objTwo[key]) {
+      return true
+    }
+   }
+ }
+ return false
 }
+
+const stooge = {name: 'moe', age: 32};
+isMatch(stooge, {age: 32});
 
 module.exports = isMatch
